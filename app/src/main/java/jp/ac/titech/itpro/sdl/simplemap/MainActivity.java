ecbc048b940437559d29842e141459baa3a83545
Takuo WATANABE <takuo@acm.org>
2016-05-19 18:33:31 +0900

    Shows the current location on the map

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
@@ -1,17 +1,42 @@
 package jp.ac.titech.itpro.sdl.simplemap;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
-    private final static LatLng MY_LOCATION = new LatLng(35.604667, 139.682759);
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+    private final static String TAG = "MainActivity";
+
+    private GoogleMap googleMap;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private boolean requestingLocationUpdate;
+
+    private final static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private final static int REQCODE_PERMISSIONS = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +46,104 @@ public class MainActivity extends AppCompatActivity {
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap map) {
-                map.addMarker(new MarkerOptions().position(MY_LOCATION).title(getString(R.string.my_location)));
-                map.moveCamera(CameraUpdateFactory.newLatLng(MY_LOCATION));
+                googleMap = map;
             }
         });
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+        if (googleApiClient.isConnected())
+            startLocationUpdate(true);
+        else
+            requestingLocationUpdate = true;
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        stopLocationUpdate();
+        requestingLocationUpdate = false;
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        if (requestingLocationUpdate)
+            startLocationUpdate(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspented");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        googleMap.animateCamera(CameraUpdateFactory
+                .newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode,
+                                           @NonNull String[] permissions, @NonNull int[] grants) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        switch (reqCode) {
+        case REQCODE_PERMISSIONS:
+            startLocationUpdate(false);
+            break;
+        }
+    }
+
+    private void startLocationUpdate(boolean reqPermission) {
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                if (reqPermission)
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQCODE_PERMISSIONS);
+                else
+                    Toast.makeText(this, getString(R.string.toast_requires_permission, permission),
+                            Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    private void stopLocationUpdate() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
     }
 }
